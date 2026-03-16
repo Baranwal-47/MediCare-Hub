d0e83404ed9caa90445f4046b1d6e18530ae0423
Utkarsh Baranwal <utkarshbaranwal47@gmail.com>
2026-03-17 03:33:47 +0530

    fix: apply today date filter using aggregate in getTokensForToday

--- server/controllers/retrieveToken.js
+++ server/controllers/retrieveToken.js
@@ -1,20 +1,32 @@
-//To Retrieve All The Tokens From The Collection Of That Particular Date i.e Present Date
-
 const { Token } = require("../models/tokModel");
 
 const getTokensForToday = async (req, res) => {
-    try {
-        const todayStart = new Date();
-        todayStart.setHours(0, 0, 0, 0);
+  try {
+    const todayStart = new Date();
+    todayStart.setHours(0, 0, 0, 0);
 
-        const todayEnd = new Date();
-        todayEnd.setHours(23, 59, 59, 999);
-        const tokensForToday = await Token.find();
+    const todayEnd = new Date();
+    todayEnd.setHours(23, 59, 59, 999);
 
-        res.status(200).json({ tokens: tokensForToday });
-    } catch (err) {
-        return res.status(500).json({ message: "Server Error For Retrieving Tokens " + err });
-    }
+    const result = await Token.aggregate([
+      { $unwind: "$tokens" },
+      {
+        $match: {
+          "tokens.createdAt": { $gte: todayStart, $lte: todayEnd },
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          tokens: { $push: "$tokens" },
+        },
+      },
+    ]);
+
+    res.status(200).json({ tokens: result });
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error For Retrieving Tokens " + err });
+  }
 };
 
 module.exports = { getTokensForToday };
